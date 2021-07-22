d9e93c7fe0d34ef324bac2564c2ab94036034e08
Tomas Kraus <tomas.kraus.dev@gmail.com>
2021-07-23 00:42:41 +0200

    1st working version

---
+++ ble-midi-box.ino
@@ -0,0 +1,97 @@
+#include <BLEMIDI_Transport.h>
+
+#include <hardware/BLEMIDI_ESP32_NimBLE.h>
+//#include <hardware/BLEMIDI_ESP32.h>
+//#include <hardware/BLEMIDI_nRF52.h>
+//#include <hardware/BLEMIDI_ArduinoBLE.h>
+
+BLEMIDI_CREATE_INSTANCE("tomikMidi", MIDI)
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
+bool isConnected = false;
+
+bool state;
+bool prevState;
+
+const int buttonPin = 4;
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void setup()
+{
+  MIDI.begin();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  BLEMIDI.setHandleConnected(OnConnected);
+  BLEMIDI.setHandleDisconnected(OnDisconnected);
+
+  MIDI.setHandleNoteOn(OnNoteOn);
+  MIDI.setHandleNoteOff(OnNoteOff);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void loop()
+{
+  MIDI.read();
+
+  if (isConnected)
+  {
+    state = digitalRead(buttonPin) == HIGH;
+    if (state != prevState) {
+      if (state == true)
+      {
+        MIDI.sendControlChange(64, 127, 1);
+        //digitalWrite(LED_BUILTIN, HIGH);
+      }
+      else
+      {
+        MIDI.sendControlChange(64, 0, 1);
+        //digitalWrite(LED_BUILTIN, LOW);
+      }
+    }
+    prevState = state;
+    delay(4);
+  }
+}
+
+// ====================================================================================
+// Event handlers for incoming MIDI messages
+// ====================================================================================
+
+// -----------------------------------------------------------------------------
+// Device connected
+// -----------------------------------------------------------------------------
+void OnConnected() {
+  isConnected = true;
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+// -----------------------------------------------------------------------------
+// Device disconnected
+// -----------------------------------------------------------------------------
+void OnDisconnected() {
+  isConnected = false;
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// -----------------------------------------------------------------------------
+// Received note on
+// -----------------------------------------------------------------------------
+void OnNoteOn(byte channel, byte note, byte velocity) {
+}
+
+// -----------------------------------------------------------------------------
+// Received note off
+// -----------------------------------------------------------------------------
+void OnNoteOff(byte channel, byte note, byte velocity) {
+}
