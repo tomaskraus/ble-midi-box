8081b5113819f4222327cb8dbda85aa894d92522
Tomas Kraus <tomas.kraus.dev@gmail.com>
2021-07-23 19:54:07 +0200

    sends pedal MIDI after BT connection is established

--- ble-midi-box.ino
+++ ble-midi-box.ino
@@ -1,3 +1,8 @@
+/*
+   Bluetooth MIDI controller
+   - sustain pedal
+*/
+
 #include <BLEMIDI_Transport.h>
 
 #include <hardware/BLEMIDI_ESP32_NimBLE.h>
@@ -9,20 +14,33 @@
 #define LED_BUILTIN 2
 #endif
 
-
 const int LOOP_DELAY = 4; //ms
+const int START_LOOP_DELAY = 1500; //ms
+bool startLoopFlag;
+
+// sustain pedal ---------------------------
+const int PEDAL_PIN = 4;
+const int LED_PEDAL = 5;
+
+typedef enum {
+  PEDAL_RESET = -1,
+  PEDAL_NOT_ACTIVE = 0,
+  PEDAL_ACTIVE = 1,
+} pedalStateEnum;
+
+pedalStateEnum pedalState;
+pedalStateEnum prevPedalState;
+// -----------------------------------------
 
 const int BT_LED = LED_BUILTIN;
 const int BT_PAIR_DELAY = 300; //ms
-
-const int PEDAL_PIN = 4;
-const int LED_PEDAL = 5;
-bool pedalState;
-bool prevPedalState;
-
 bool isConnectedBT = false;
 
-const byte MIDI_CH = 1;
+const byte MIDI_CH = 1; //MIDI output channel
+
+// ----------------------------------------------------------------------------
+
+
 BLEMIDI_CREATE_INSTANCE("ble-midi-box", MIDI)
 
 // -----------------------------------------------------------------------------
@@ -52,13 +70,19 @@ void loop()
 
   if (isConnectedBT)
   {
-    pedalState = isPedalActive();
-    if (pedalState != prevPedalState) 
+    if (startLoopFlag) {
+      digitalWrite(LED_PEDAL, LOW);
+      delay(START_LOOP_DELAY);  //wait to ensure MIDI communication will work
+      startLoopFlag = false;
+    }
+
+    pedalState = toPedalState(isPedalActive());
+    if (pedalState != prevPedalState)
     {
-      setPedal(pedalState, MIDI_CH);
+      sendPedalMidi(pedalStateToBool(pedalState), MIDI_CH);
       prevPedalState = pedalState;
     }
-    
+
     delay(LOOP_DELAY);
   }
   else
@@ -80,7 +104,18 @@ bool isPedalActive() {
   return digitalRead(PEDAL_PIN) == HIGH;
 }
 
-void setPedal(bool active, byte midiChannel) {
+pedalStateEnum toPedalState(bool state) {
+  return state
+         ? PEDAL_ACTIVE
+         : PEDAL_NOT_ACTIVE
+         ;
+}
+
+boolean pedalStateToBool(pedalStateEnum pState) {
+  return pState == PEDAL_ACTIVE;
+}
+
+void sendPedalMidi(bool active, byte midiChannel) {
   if (active) {
     MIDI.sendControlChange(64, 127, midiChannel);
     digitalWrite(LED_PEDAL, HIGH);
@@ -88,6 +123,10 @@ void setPedal(bool active, byte midiChannel) {
     MIDI.sendControlChange(64, 0, midiChannel);
     digitalWrite(LED_PEDAL, LOW);
   }
+}
+
+void forcePedalStateRefresh() {
+  prevPedalState = PEDAL_RESET;
 }
 
 // ====================================================================================
@@ -101,7 +140,8 @@ void OnConnected() {
   isConnectedBT = true;
   digitalWrite(BT_LED, HIGH);
 
-  setPedal(isPedalActive(), MIDI_CH);
+  startLoopFlag = true;
+  forcePedalStateRefresh();
 }
 
 // -----------------------------------------------------------------------------
